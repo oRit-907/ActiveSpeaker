cb04c711b10da2580cefb2b008a3d143066cc4ea
oRit_907 <165120130+oRit-907@users.noreply.github.com>
2025-05-27 20:03:19 -0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,20 +1,33 @@
 local playerNames = {}
 
-AddEventHandler('playerDropped', function()
+-- Handle player disconnects
+AddEventHandler('playerDropped', function(reason)
     local src = source
     playerNames[src] = nil
-    TriggerClientEvent('txylor:updateNames', -1, playerNames)
+    TriggerClientEvent('orit:updateNames', -1, playerNames)
 end)
 
-RegisterNetEvent('txylor:requestNames')
-AddEventHandler('txylor:requestNames', function()
+-- Handle name request from client
+RegisterNetEvent('orit:requestNames', function()
     local src = source
     local identifier = GetPlayerIdentifier(src, 0)
-    MySQL.Async.fetchScalar('SELECT CONCAT(firstname, " ", lastname) FROM users WHERE identifier = @identifier', {
+
+    if not identifier then
+        print(("Failed to get identifier for player %d"):format(src))
+        return
+    end
+
+    -- Fetch full name from database (fallback to game name if not found)
+    MySQL.Async.fetchScalar([[
+        SELECT CONCAT(firstname, " ", lastname) FROM users WHERE identifier = @identifier
+    ]], {
         ['@identifier'] = identifier
     }, function(fullname)
-        if not fullname then fullname = GetPlayerName(src) end
+        if not fullname or fullname == "" then
+            fullname = GetPlayerName(src)
+        end
+
         playerNames[src] = fullname
-        TriggerClientEvent('txylor:updateNames', -1, playerNames)
+        TriggerClientEvent('orit:updateNames', -1, playerNames)
     end)
 end)
